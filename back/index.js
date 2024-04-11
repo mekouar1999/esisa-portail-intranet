8a4cbefb61d1b0b6db195fc6ecfa352748c0ac60
Mekouar1999 <othman.mekouar99@gmail.com>
2024-04-11 06:53:40 -0700

    Hello World from Server Side

--- back/index.js
+++ back/index.js
@@ -3,3 +3,11 @@ const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
+
+app.use('/',(req,res)=>{
+    res.send('Hello World from Server Side')
+})
+app.listen(PORT,() =>{
+    console.log(`Serveur est en cours d'execution dans le port ${PORT}`)
+})
+
