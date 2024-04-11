8eb6a2ea3e99d51594f8038507397a66ccd5549a
Mekouar1999 <othman.mekouar99@gmail.com>
2024-04-11 06:40:48 -0700

    test

---
+++ front/src/Components/ConnexionPage/InterfaceDeConnexion.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "../../App.css"
+
+const InterfaceDeConnexion = () => {
+  return (
+    <div className="test">
+
+
+
+    <h1 className="h1">Interface de connexion</h1>
+
+
+    <h1>Username : </h1>
+    <input></input>
+
+
+    <h1>Password : </h1>
+    <input></input>
+    <button>ok</button>
+    </div>
+  )
+}
+
+export default InterfaceDeConnexion
